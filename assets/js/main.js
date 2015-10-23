9d7afb9e6f0f6803635b2e7dffb491456d175386
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-23 16:22:20 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("#btext").mouseenter(function(){
+	$("#btext").text("Thank You :) ");
+	}).mouseleave( function(){
+	$("#btext").text("Sign The Petition!");
+	});
+
+});
+
+
+
+
+
